e8e1cb7e70232b0b2487c61d82bc92772528e221
hitwlh <347813864@qq.com>
2014-11-30 19:37:23 +0800

    修改子功能

--- src/study.java
+++ src/study.java
@@ -1,20 +1,34 @@
-public class study {
-	private String org;
-	private String substance;
+
+public class funds {
+	private String name;
+	private String source;
+	private String leader;
 	private String start;
 	private String end;
+	private double contract;
+	private double already;
+	private double non;
+	private int number;
 	private String person;
-	public String getOrg() {
-		return org;
+	private int per_level;
+	private String type;
+	public String getName() {
+		return name;
 	}
-	public void setOrg(String org) {
-		this.org = org;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public String getSubstance() {
-		return substance;
+	public String getSource() {
+		return source;
 	}
-	public void setSubstance(String substance) {
-		this.substance = substance;
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getLeader() {
+		return leader;
+	}
+	public void setLeader(String leader) {
+		this.leader = leader;
 	}
 	public String getStart() {
 		return start;
@@ -28,10 +42,46 @@ public class study {
 	public void setEnd(String end) {
 		this.end = end;
 	}
+	public double getContract() {
+		return contract;
+	}
+	public void setContract(double contract) {
+		this.contract = contract;
+	}
+	public double getAlready() {
+		return already;
+	}
+	public void setAlready(double already) {
+		this.already = already;
+	}
+	public double getNon() {
+		return non;
+	}
+	public void setNon(double non) {
+		this.non = non;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
 	public String getPerson() {
 		return person;
 	}
 	public void setPerson(String person) {
 		this.person = person;
+	}
+	public int getPer_level() {
+		return per_level;
+	}
+	public void setPer_level(int per_level) {
+		this.per_level = per_level;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
 	}
 }
