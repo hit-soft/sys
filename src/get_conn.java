0652d5ea11a99a49c3291e300b6b7cece32a22bf
hitwlh <347813864@qq.com>
2014-12-05 23:19:15 +0800

    修复bug，减少跳转页面

--- src/get_conn.java
+++ src/get_conn.java
@@ -1,23 +1,40 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
-public class get_conn {
-	public void closeConnection(Connection conn){
-		if(conn!=null)
-			try {
-				conn.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-	}
-	public Connection getConnection(){
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class export {
+	public String Export(){
+		get_conn util=new get_conn();
+		Connection conn=util.getConnection();
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/project",
-					"root","1234");
-		} catch (Exception e) {
+			System.out.println("ÍõÁú»ªÍõÁú»ªÍõÁú»ªÍõÁú»ªÍõÁú»ªÍõÁú»ªÍõÁú»ª");
+			Statement stmt=conn.createStatement();
+			String sql="select * from priz into outfile 'D://qpriz.xls' ";
+			stmt.execute(sql);
+			sql="select * from accept into outfile 'D://qaccept.xls' ";
+			stmt.execute(sql);
+			sql="select * from coope into outfile 'D://qcoope.xls' ";
+			stmt.execute(sql);
+			sql="select * from funds into outfile 'D://qfunds.xls' ";
+			stmt.execute(sql);
+			sql="select * from patent into outfile 'D://qpatent.xls' ";
+			stmt.execute(sql);
+			sql="select * from publ into outfile 'D://qpubl.xls' ";
+			stmt.execute(sql);
+			sql="select * from software into outfile 'D://qsoftware.xls' ";
+			stmt.execute(sql);
+			sql="select * from study into outfile 'D://qstudy.xls' ";
+			stmt.execute(sql);
+			sql="select * from team into outfile 'D://qteam.xls' ";
+			stmt.execute(sql);
+			return "1";
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return "0";
+		}finally{
+			util.closeConnection(conn);
 		}
-		return null;
-		}
+	}
 }
