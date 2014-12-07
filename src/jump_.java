6bb3483c23ea0945b4b8eca3b3f0abb275e1bba9
hitwlh <347813864@qq.com>
2014-12-07 18:49:17 +0800

    修复bug，完善功能

--- src/jump_.java
+++ src/jump_.java
@@ -1,31 +1,87 @@
 
-public class jump_ {
-	private String fuck;
-	private int flag;
-	private String per;
-	public String getFuck() {
-		return fuck;
+public class funds {
+	private String name;
+	private String source;
+	private String leader;
+	private String start;
+	private String end;
+	private double contract;
+	private double already;
+	private double non;
+	private int number;
+	private String person;
+	private int per_level;
+	private String type;
+	public String getName() {
+		return name;
 	}
-	public void setFuck(String fuck) {
-		this.fuck = fuck;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public int getFlag() {
-		return flag;
+	public String getSource() {
+		return source;
 	}
-	public void setFlag(int flag) {
-		this.flag = flag;
+	public void setSource(String source) {
+		this.source = source;
 	}
-	public String getPer() {
-		return per;
+	public String getLeader() {
+		return leader;
 	}
-	public void setPer(String per) {
-		this.per = per;
+	public void setLeader(String leader) {
+		this.leader = leader;
 	}
-	public String change(){
-		return "1";
+	public String getStart() {
+		return start;
 	}
-	public String jump_(){
-		System.out.println("fuck="+fuck);
-		return fuck;
+	public void setStart(String start) {
+		this.start = start;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+	public double getContract() {
+		return contract;
+	}
+	public void setContract(double contract) {
+		this.contract = contract;
+	}
+	public double getAlready() {
+		return already;
+	}
+	public void setAlready(double already) {
+		this.already = already;
+	}
+	public double getNon() {
+		return non;
+	}
+	public void setNon(double non) {
+		this.non = non;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getPerson() {
+		return person;
+	}
+	public void setPerson(String person) {
+		this.person = person;
+	}
+	public int getPer_level() {
+		return per_level;
+	}
+	public void setPer_level(int per_level) {
+		this.per_level = per_level;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
 	}
 }
