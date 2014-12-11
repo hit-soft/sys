d82c278c2250a301d57ef4476836384270939f78
hitwlh <347813864@qq.com>
2014-12-11 23:15:23 +0800

    完善代码，减少冗余

--- src/per.java
+++ src/per.java
@@ -1,10 +1,45 @@
-
-public class per {
-	private String per;
-	public String getPer() {
-		return per;
+-
+public class priz {
+	private String name;
+	private String type;
+	private String level;
+	private String time;
+	private String person;
+	private int per_level;
+	public String getName() {
+		return name;
 	}
-	public void setPer(String per) {
-		this.per = per;
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getPerson() {
+		return person;
+	}
+	public void setPerson(String person) {
+		this.person = person;
+	}
+	public int getPer_level() {
+		return per_level;
+	}
+	public void setPer_level(int per_level) {
+		this.per_level = per_level;
 	}
 }
